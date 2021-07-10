013f68b98a6678734d50285af13656611e2c6ef5
ustccuikai <ustccuikai@126.com>
2021-07-10 21:21:08 +0800

    sync tcx format file from strava to garmin

--- scripts/strava_to_garmin_sync.py
+++ scripts/strava_to_garmin_sync.py
@@ -144,16 +144,16 @@ if __name__ == "__main__":
     )
     loop.run_until_complete(future)
 
-    garmin_client_nrc = Garmin(
-        options.garmin_email_nrc, options.garmin_password_nrc, garmin_auth_domain
-    )
-    loop = asyncio.get_event_loop()
-    future = asyncio.ensure_future(
-        upload_to_activities(
-            garmin_client_nrc, strava_client, strava_web_client, DataFormat.TCX
-        )
-    )
-    loop.run_until_complete(future)
+    # garmin_client_nrc = Garmin(
+    #     options.garmin_email_nrc, options.garmin_password_nrc, garmin_auth_domain
+    # )
+    # loop = asyncio.get_event_loop()
+    # future = asyncio.ensure_future(
+    #     upload_to_activities(
+    #         garmin_client_nrc, strava_client, strava_web_client, DataFormat.TCX
+    #     )
+    # )
+    # loop.run_until_complete(future)
 
     # Run the strava sync
     run_strava_sync(
